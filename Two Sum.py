bf769362af182e565c34c426dc91267d7feb4a34
Niranjan R <83063386+Niranjan7407@users.noreply.github.com>
2025-04-06 14:46:34 +0530

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic={}
+        for index,num in enumerate(nums):
+            if num in dic:
+                return [dic[num],index]
+            dic[target-num]=index
+        
+       
+
+
+
